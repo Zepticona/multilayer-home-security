9ad7f1043c1a505554bc4f67b2cf2eba1846811b
zepticona <isakil416@gmail.com>
2023-01-04 10:17:17 +0600

    testing...

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
+document.querySelector('.password-field').style.display='none';
+document.querySelector('.succes-message').style.display='none';
+document.querySelector('.fail-message').style.display='none';
 
-document.querySelector('.succes-message').style.display='none';        
-document.querySelector('.fail-message').style.display='none'; 
-// document.querySelector('.warning-text').style.display='none';
-let data;
-let passwordMatched;
 fetch('https://api.thingspeak.com/channels/1996273/feeds.json?api_key=0JYUOL5Q31LMBMFK&results=2')
 .then(res => res.json())
 .then(data => {
-    passwordMatched = data.feeds[1].field5;
-    console.log(passwordMatched);
-    if(passwordMatched == 1) {
-        console.log('capturing');
-        document.querySelector('.status-standby').style.display = 'none';
-        document.querySelector('.status-capturing').style.display = 'inline-block';
-    } else if(passwordMatched == 0) {
-        console.log('stadning by');
-        document.querySelector('.status-standby').style.display = 'inline-block';
-        document.querySelector('.status-capturing').style.display = 'none';
+    waitingPassword = data.feeds[1].field6;
+    console.log(waitingPassword);
+    if(waitingPassword == 1) {
+        document.querySelector('.password-field').style.display = 'inline-block';
+        document.querySelector('.fingerprint-notifier').style.display = 'none';
+    } else if(waitingPassword == 0) {
+        document.querySelector('.password-field').style.display = 'none';
+        document.querySelector('.fingerprint-notifier').style.display = 'inline-block';
     }
 });
+
+// document.querySelector('.warning-text').style.display='none';
+let databaseData;
+let waitingPassword;
 
 document.querySelector('.form').addEventListener('submit', (e) => {
     e.preventDefault();
 
     let requiredPassword;
-    const fingerprintId = data.currentEntrance;
+    const fingerprintId = databaseData.currentEntrance;
 
-    for(let user in data.users) {
-        if(data.users[user].id == fingerprintId) {
-            console.log('Fingerprint user found with id: '+data.users[user].id);
-            requiredPassword = data.users[user];
+    for(let user in databaseData.users) {
+        if(databaseData.users[user].id == fingerprintId) {
+            console.log('Fingerprint user found with id: '+databaseData.users[user].id);
+            requiredPassword = databaseData.users[user].password;
         } else {
-            console.log(data.users[user].id + " is not equal to " + fingerprintId )
+            console.log(databaseData.users[user].id + " is not equal to " + fingerprintId )
         }
     }
 
     const userInput = Object.fromEntries(new FormData(e.target).entries());
     if(userInput['password']==requiredPassword) {
-        console.log('password matched');
-        fetch('https://api.thingspeak.com/update?api_key=8GH5FISIM2GY8Z4L&field5=1')
+        fetch('https://api.thingspeak.com/update?api_key=8GH5FISIM2GY8Z4L&field5=0&field6=0&field7=1')
         .then(res => res.json())
         .then(data => {        
             document.querySelector('.succes-message').style.display='block';        
             document.querySelector('.fail-message').style.display='none';        
         });
     } else {
-        fetch('https://api.thingspeak.com/update?api_key=8GH5FISIM2GY8Z4L&field5=0')
+        fetch('https://api.thingspeak.com/update?api_key=8GH5FISIM2GY8Z4L&field5=1&field6=0&field7=0')
         .then(res => res.json())
         .then(data => {        
             document.querySelector('.succes-message').style.display='none';        
@@ -58,33 +57,41 @@ document.querySelector('.form').addEventListener('submit', (e) => {
 
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-      const firebaseConfig = {
-        apiKey: "AIzaSyCyUL_n_R-BL0mQpBRXhFJ7I5jl0R82YgA",
-        authDomain: "fingerprint-18576.firebaseapp.com",
-        databaseURL: "https://fingerprint-18576-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "fingerprint-18576",
-        storageBucket: "fingerprint-18576.appspot.com",
-        messagingSenderId: "343580357576",
-        appId: "1:343580357576:web:3f7fbb9c915c293a6446fd"
-      };
-      // Initialize Firebase
-      const app = initializeApp(firebaseConfig);
-      import {getDatabase,get,  ref, set, child, update, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const firebaseConfig = {
+apiKey: "AIzaSyCyUL_n_R-BL0mQpBRXhFJ7I5jl0R82YgA",
+authDomain: "fingerprint-18576.firebaseapp.com",
+databaseURL: "https://fingerprint-18576-default-rtdb.asia-southeast1.firebasedatabase.app",
+projectId: "fingerprint-18576",
+storageBucket: "fingerprint-18576.appspot.com",
+messagingSenderId: "343580357576",
+appId: "1:343580357576:web:3f7fbb9c915c293a6446fd"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+import {getDatabase,get,  ref, set, child, update, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-      const db = getDatabase();
+const db = getDatabase();
 
-      function getData() {
-        const dbref = ref(db);
+function getData() {
+const dbref = ref(db);
 
-        get(child(dbref, "/"))
-        .then( snapshot => {          
-          console.log(snapshot);
-          if(snapshot.exists()) {
-            data = snapshot.val();
-            console.log(data); 
-            
-          } 
-        })
-      }
+get(child(dbref, "/"))
+.then( snapshot => {          
+    console.log(snapshot);
+    if(snapshot.exists()) {
+    databaseData = snapshot.val();
+    console.log(databaseData); 
+    const fingerprintId = databaseData.currentEntrance;
+    for(let user in databaseData.users) {
+        if(databaseData.users[user].id == fingerprintId) {
+            document.querySelector('.username').insertAdjacentText('beforeend', user);
+        } else {
+            console.log(databaseData.users[user].id + " is not equal to " + fingerprintId )
+        }
+    }
+    
+    } 
+})
+}
 
-      window.onload = getData
+window.onload = getData;
